3b5024a8b4d5488f5d502e881280da2b9cdbf594
FlavioFerreira <45465841+flavioafferreira@users.noreply.github.com>
2023-06-03 01:57:38 +0200

    Case- Updated

--- includes/accessories/special.c
+++ includes/accessories/special.c
@@ -767,7 +767,7 @@ void lorawan_tx_data(void){
             color(255);
             //lorawan_reconnect_cnt=0;
 		     }
-    if(data_sent_cnt>=DATA_SENT_JOIN_AGAIN){lorawan_reconnect=1;data_sent_cnt=0;}
+    if(data_sent_cnt>=DATA_SENT_JOIN_AGAIN){lorawan_reconnect=1;}
 
   
 
